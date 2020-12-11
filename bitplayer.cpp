395b22b9437e256f150a1e188d14d42288341aa6
MiroChao <531414952@qq.com>
2020-12-11 10:20:43 +0800

    test fix bitplayer for V2

--- bitplayer.cpp
+++ bitplayer.cpp
@@ -7,17 +7,36 @@ namespace bitplayer {
     //%
     void init() {
         if (initialized) return;
+        
+        #if MICROBIT_CODAL
+            Button  buttonP5;
+            Button  buttonP11;
+            
+            buttonP5(io.P5, DEVICE_ID_IO_P5, DEVICE_BUTTON_ALL_EVENTS, ACTIVE_LOW);
+            buttonP11(io.P11, DEVICE_ID_IO_P11, DEVICE_BUTTON_ALL_EVENTS, ACTIVE_LOW);
 
+
+        #else
+            #define ALLOC_PIN_BUTTON(id) new MicroBitButton(getPin(id)->name, id, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
+                ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P5)
+                ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P11)
+                ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P12)
+                ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P13)
+                ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P14)
+                ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P15)
+                ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P16)
+            #undef ALLOC_PIN_BUTTON
+        #endif
     // mount buttons on the pins with a pullup mode
     // TODO: fix this issue in the DAL itself
-#define ALLOC_PIN_BUTTON(id) new MicroBitButton(getPin(id)->name, id, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
-    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P13)
-    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P14)
-    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P15)
-    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P5)
-    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P11)
-    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P16)
-#undef ALLOC_PIN_BUTTON
+        //#define ALLOC_PIN_BUTTON(id) new MicroBitButton(getPin(id)->name, id, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
+        //   ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P13)
+        //    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P14)
+        //    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P15)
+        //    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P5)
+        //    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P11)
+        //    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P16)
+        //#undef ALLOC_PIN_BUTTON
 
         initialized = true;
     }
